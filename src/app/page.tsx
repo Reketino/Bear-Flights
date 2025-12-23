ccff2e692afd436f410576c769277dc9685e92f2
Reketino <bjornevensk8@gmail.com>
2025-12-23 22:32:49 +0100

    HomePage setup started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
-export default function Page() {
+export default function HomePage() {
   return (
-    <main>
-      <h1>Bear Flight Tracking ✈️🐻</h1>
-      <p>System is live.</p>
+    <main className="
+    min-h-[calc(100vh-3.5rem)]
+    flex items-center
+    justify-center px-6
+    ">
+      <section className="
+      max-w-3xl text-center
+      ">
+      <h1 className="text-5xl font-extrabold mb-6">
+       🐻 Bear Flight Tracking ✈️
+        </h1>
+
+      <p className="text-neutral-500 text-lg mb-8">
+        Overview of flights crossing the airspace
+        in Sykkylven, Norway - updates every 5 minutes.
+        </p>
+      </section>
     </main>
   );
 }
