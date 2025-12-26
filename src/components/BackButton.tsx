32c570ac96faf3408b8aa5151cb22e9cd4a05581
Reketino <bjornevensk8@gmail.com>
2025-12-27 00:02:37 +0100

    made back button component for easier navigation

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useRouter } from "next/router";
+
+export default function BackButton() {
+    const router = useRouter();
+  return (
+    <div>
+      
+    </div>
+  )
+}
