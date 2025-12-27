2fc348b9b0e84069ab8f5b994785466315697da5
Reketino <bjornevensk8@gmail.com>
2025-12-27 11:03:05 +0100

    made backbutton for pages

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,11 +1,33 @@
 "use client";
-import { useRouter } from "next/router";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function BackButton() {
     const router = useRouter();
+
+
   return (
-    <div>
-      
-    </div>
+    <button
+    onClick={() => router.back()}
+    aria-label="Go back"
+    className="
+    inline-flex items-center gap-2
+    px-3 py-1 rounded-lg
+    text-sm text-sky-400
+    bg-black/20 hover:bg-black/30
+    border border-white/10
+    mb-4
+    ">
+      <Image
+      src="/icons/backicon.png"
+      alt="plane back"
+      width={20}
+      height={20}
+      className="
+      transition-transform duration-200
+      group-hover:-translate-x-1
+      "
+      />
+    </button>
   )
 }
