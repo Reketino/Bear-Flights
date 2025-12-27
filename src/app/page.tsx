9af954c594d1eaea7afac921be3d97e5cd49734a
Reketino <bjornevensk8@gmail.com>
2025-12-27 19:57:21 +0100

    Daily flights-page up and 🏃🏻‍➡️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,68 +3,91 @@ import AnimatedLogo from "@/components/animatedlogo";
 
 export default function HomePage() {
   return (
-    <main className="
+    <main
+      className="
     min-h-[calc(100vh-3.5rem)]
     flex items-center
     justify-center px-6
-    ">
-      <section className="
+    "
+    >
+      <section
+        className="
       max-w-3xl text-center
-      ">
+      "
+      >
         <AnimatedLogo />
-        
 
-      <p className="text-neutral-500 text-lg mb-8">
-        Overview of flights crossing the airspace
-        in Sykkylven, Norway - updates every 5 minutes.
+        <p className="text-neutral-500 text-lg mb-8">
+          Overview of flights crossing the airspace in Sykkylven, Norway -
+          updates every 5 minutes.
         </p>
 
-       
-      <nav className=" 
+        <nav
+          className=" 
       flex flex-col
       justify-center gap-4
-      ">
-      <Link
-      href="/flights"
-      className="
+      "
+        >
+          {/* Daily Flights Page */}
+          <Link
+            href="/daily-flights"
+            className="
+        px-6 py-3 rounded-xl
+        text-black font-semibold
+        bg-emerald-500 hover:bg-emerald-400
+        transition
+        "
+          >
+            Flights Today📅
+          </Link>
+
+          {/* All Registered Flights */}
+          <Link
+            href="/flights"
+            className="
       px-6 py-3 rounded-xl
       text-black font-semibold
       bg-amber-500 hover:bg-amber-400
       transition
-      ">
-        Take a look at historical flights!✈️
-      </Link>
-      <Link
-      href="/flights/map"
-      className=" px-6 py-3 rounded-xl
+      "
+          >
+            Take a look at historical flights!✈️
+          </Link>
+
+          {/* Map Showing flights */}
+          <Link
+            href="/flights/map"
+            className=" px-6 py-3 rounded-xl
       text-black font-semibold
       bg-blue-500 hover:bg-blue-400
       transition
-      ">
-        Flight Map🗺️
-      </Link>
+      "
+          >
+            Flight Map🗺️
+          </Link>
 
-
-      <a
-      href="https://opensky-network.org"
-      target="_blank"
-      className="
+          <a
+            href="https://opensky-network.org"
+            target="_blank"
+            className="
       px-3 py-3 rounded-xl
       text-neutral-200 font-semibold
       
       border border-amber-500/50
       hover:bg-white/10 transition
-      ">
-      Datasource: Opensky
-      </a>
-      </nav>
+      "
+          >
+            Datasource: Opensky
+          </a>
+        </nav>
 
-
-      <p className="
+        <p
+          className="
       text-xs text-neutral-500 mt-10
-      ">
-        Buildt using Next.js, Supabase & Opensky API 
-      </p>
+      "
+        >
+          Buildt using Next.js, Supabase & Opensky API
+        </p>
       </section>
     </main>
   );
