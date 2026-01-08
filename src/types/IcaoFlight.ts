06d5418424b47d91612f88a755f67e8dcb5d3ad8
Reketino <bjornevensk8@gmail.com>
2026-01-08 16:22:59 +0100

    Refactored Icao24 main section into own component

---
+++ src/types/IcaoFlight.ts
@@ -0,0 +1,10 @@
+export type IcaoFlight = {
+    id?: number;
+    icao24: string;
+    callsign: string | null;
+    origin: string | null;
+    route: string | null;
+    distance_over_area: number | null;
+    first_seen: string;
+    last_seen: string;
+}
