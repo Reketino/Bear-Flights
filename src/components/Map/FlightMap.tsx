feb21513bd6fe990609e5546a73beb3f7c32a5b4
Reketino <bjornevensk8@gmail.com>
2026-01-09 16:36:00 +0100

    Formatting

--- src/components/Map/FlightMap.tsx
+++ src/components/Map/FlightMap.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { AirPort_COORDS } from "@/lib/airportcoords";
 
-
-import { MapContainer, TileLayer, Marker, Popup, Circle, Polyline, useMap, } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Circle,
+  Polyline,
+  useMap,
+} from "react-leaflet";
 import type { LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useEffect } from "react";
-
 
 type FlightPosition = {
   icao24: string;
@@ -25,19 +31,15 @@ const CENTER: LatLngExpression = [62.392497, 6.578392];
 
 // Defining heading on the map
 const safeHeading = (heading: number | null) =>
-  typeof heading === "number" && Number.isFinite(heading)
-    ? heading
-    : 0;
+  typeof heading === "number" && Number.isFinite(heading) ? heading : 0;
 
 // Altitude Color based on Altitude
 const altitudeColor = (altitude: number | null) => {
   if (altitude === null) return "#9ca3af";
   if (altitude < 3000) return "#22c55e";
-  if (altitude < 9000) return "#eab308"; 
+  if (altitude < 9000) return "#eab308";
   return "#ef4444";
 };
-
-
 
 // Autpan when entering flight from Icao24
 function AutoPan({ flights }: { flights: FlightPosition[] }) {
@@ -47,31 +49,26 @@ function AutoPan({ flights }: { flights: FlightPosition[] }) {
     if (flights.length === 1) {
       const f = flights[0];
 
-      map.flyTo(
-        [f.latitude, f.longitude],
-        11,
-        {
-          animate: true,
-          duration: 1.5,
-          easeLinearity: 0.25,
-        }
-      );
+      map.flyTo([f.latitude, f.longitude], 11, {
+        animate: true,
+        duration: 1.5,
+        easeLinearity: 0.25,
+      });
     }
   }, [flights, map]);
 
   return null;
 }
 
-export default function FlightMap({ 
-  flights, 
+export default function FlightMap({
+  flights,
   singleFlight,
-}: { 
+}: {
   flights: FlightPosition[];
-  singleFlight?: boolean; 
+  singleFlight?: boolean;
 }) {
-
   const planeIcon = (heading: number | null, altitude: number | null) => {
-    const rotation = safeHeading(heading)- 90;
+    const rotation = safeHeading(heading) - 90;
 
     return L.divIcon({
       className: "",
@@ -101,110 +98,102 @@ export default function FlightMap({
     });
   };
 
-
-  
-
   return (
     <section className=" relative">
-    
-    {singleFlight && (
-    <div className="absolute top-4 right-4 z-1000">
-      <a
-      href="/flights/map"
-      title="Overview"
-      className="
+      {singleFlight && (
+        <div className="absolute top-4 right-4 z-1000">
+          <a
+            href="/flights/map"
+            title="Overview"
+            className="
       inline-flex items-center gap-2
       px-4 py-2 rounded-lg 
       bg-slate-800 hover:bg-slate-700
       text-slate-100 font-semibold
       transition
       "
-      >
-       ← Return to overview 
-      </a>
-    </div>
-  )}
-
-    <MapContainer
-      center={CENTER} // Sykkylven Center
-      zoom={8}
-      className="h-150 w-full rounded-xl"
-    >
-
-      <AutoPan flights={flights} />
-
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-
-      {!singleFlight && (
-      <Circle
-        {...({
-          center: CENTER,
-          radius: 50_000,
-          pathOptions: {
-            color: "#38bdf8",
-            fillOpacity: 0.05,
-          },
-        } as any)}
-      />
+          >
+            ← Return to overview
+          </a>
+        </div>
       )}
 
+      <MapContainer
+        center={CENTER} // Sykkylven Center
+        zoom={8}
+        className="h-150 w-full rounded-xl"
+      >
+        <AutoPan flights={flights} />
 
-      {/* Departure Airport */}
-      {singleFlight &&
-      flights.map((f) => {
-        if (!f.departure_airport) return null;
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      const origin = AirPort_COORDS[f.departure_airport];
-        if (!origin) return null;
-
-        return (
-          <Polyline
-          key={`${f.icao24}-line`}
-          positions={[
-            [origin.lat, origin.lon],
-            [f.latitude, f.longitude],
-          ]}
-          pathOptions={{
-            color: "#38bdf8",
-            weight: 2,
-            dashArray: "4 6"
-          }}
+        {!singleFlight && (
+          <Circle
+            {...({
+              center: CENTER,
+              radius: 50_000,
+              pathOptions: {
+                color: "#38bdf8",
+                fillOpacity: 0.05,
+              },
+            } as any)}
           />
-        );
-      })}
+        )}
 
+        {/* Departure Airport */}
+        {singleFlight &&
+          flights.map((f) => {
+            if (!f.departure_airport) return null;
 
-      {flights.map((f) => (
-        <Marker
-          key={`${f.icao24}-${f.heading}`}
-          position={[f.latitude, f.longitude] as LatLngExpression}
-          icon={planeIcon(f.heading, f.altitude)}
-        >
-          <Popup>
-            <section
-              className="
-                text-sm"
-            >
-              <header
+            const origin = AirPort_COORDS[f.departure_airport];
+            if (!origin) return null;
+
+            return (
+              <Polyline
+                key={`${f.icao24}-line`}
+                positions={[
+                  [origin.lat, origin.lon],
+                  [f.latitude, f.longitude],
+                ]}
+                pathOptions={{
+                  color: "#38bdf8",
+                  weight: 2,
+                  dashArray: "4 6",
+                }}
+              />
+            );
+          })}
+
+        {flights.map((f) => (
+          <Marker
+            key={`${f.icao24}-${f.heading}`}
+            position={[f.latitude, f.longitude] as LatLngExpression}
+            icon={planeIcon(f.heading, f.altitude)}
+          >
+            <Popup>
+              <section
                 className="
-                    font-semibold"
+                text-sm"
               >
-                ✈️{f.callsign ?? f.icao24}
-              </header>
+                <header
+                  className="
+                    font-semibold"
+                >
+                  ✈️{f.callsign ?? f.icao24}
+                </header>
 
-              {f.heading !== null && (
-                <div>Heading: {Math.round(f.heading)}°</div>
-              )}
-              {f.altitude !== null && (
-               <div>Altitude: {Math.round(f.altitude)} m</div>
-              )}
-              {f.velocity && <div>Speed: {Math.round(f.velocity)} m/s</div>}
-            </section>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+                {f.heading !== null && (
+                  <div>Heading: {Math.round(f.heading)}°</div>
+                )}
+                {f.altitude !== null && (
+                  <div>Altitude: {Math.round(f.altitude)} m</div>
+                )}
+                {f.velocity && <div>Speed: {Math.round(f.velocity)} m/s</div>}
+              </section>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </section>
   );
 }
