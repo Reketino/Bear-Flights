8a88bde721ce1d9685ea9e9133fc386a6ce9ed4a
Reketino <bjornevensk8@gmail.com>
2026-01-09 11:50:59 +0100

    Added coordinates for AES, OSL & ARN, to airport coords

---
+++ src/lib/airportcoords.ts
@@ -0,0 +1,10 @@
+export type AirPortCoords = {
+    lat: number;
+    lon: number;
+};
+
+export const AirPort_COORDS: Record<string, AirPortCoords> = {
+    ENAL: { lat:62.5625, lon:6.119699954986572 },
+    ENGM: { lat:60.121, lon:11.0502 },
+    ESSA: { lat:59.651901245117, lon:17.918600082397 },
+}
