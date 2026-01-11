4463a7db1ca7587bcef021f312c05e750be0923e
Reketino <bjornevensk8@gmail.com>
2026-01-11 16:59:06 +0100

    Split up components into new folder, and made compnents for navbar

---
+++ src/components/nav/navbar/Brand.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Radar } from "lucide-react";
+
+export default function Brand() {
+  return (
+    <Link
+    href="/bearsflight"
+    className="
+    flex items-center group
+    gap-3 px-2 py-1
+    rounded-2xl
+    hover:bg-neutral-100
+    transition
+    ">
+      <section className="
+      grid h-10 w-10 place-items-center
+      rounded-2xl
+      border border-neutral-200
+      bg-white shadow-sm
+      ">
+        <Radar className="
+        h-5 w-5 text-sky-700" />
+      </section>
+
+      <section className="leading-tight">
+        <p className="
+        text-sm font-semibold
+        tracking-tight text-neutral-950
+        ">
+            BearFlights
+        </p>
+        <p className="text-xs text-neutral-500">
+            Air traffic dashboard
+        </p>
+      </section>
+    </Link>
+  )
+}
