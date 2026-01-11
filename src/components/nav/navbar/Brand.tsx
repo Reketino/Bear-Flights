eb63fa049f53cdd90df47b5961234c4c61b59a70
Reketino <bjornevensk8@gmail.com>
2026-01-11 20:56:06 +0100

    Deleted buttons, since navbar has taken over their roles + added logo to navbar & played around w/ colors

--- src/components/nav/navbar/Brand.tsx
+++ src/components/nav/navbar/Brand.tsx
@@ -1,27 +1,33 @@
 import Link from "next/link";
 import { Radar } from "lucide-react";
+import Image from "next/image";
 
 export default function Brand() {
   return (
     <Link
-    href="/
-    "
+    href="/"
     className="
     flex items-center group
     gap-3 px-2 py-1
     rounded-2xl
-    hover:bg-white/10
+    hover:bg-sky-400/20
     transition
     ">
       <section className="
       grid h-10 w-10 place-items-center
       rounded-2xl
-      border border-neutral-200
-      bg-white shadow-sm
+      border border-neutral-200/5
+      bg-black/15 shadow-sm
       ">
-        <Radar className="
-        h-5 w-5 text-sky-700" />
-      </section>
+        <Image
+        src="/icons/brand.png"
+        alt="Bearflights logo"
+        width={64}
+        height={64}
+        className="w-8 h-8 object-contain"
+        priority
+        />
+        </section>
 
       <section className="leading-tight">
         <p className="
