da8325eae0fbb95fd0164a06c7d35c2071089695
Reketino <bjornevensk8@gmail.com>
2026-01-11 11:37:16 +0100

    Installed lucide react & started working on navbar

---
+++ src/components/nav/NavItems.ts
@@ -0,0 +1,16 @@
+import type { LucideIcon } from "lucide-react";
+import { Radar, PlaneTakeoff, Clock, Globe2 } from "lucide-react";
+
+
+export type NavItem = {
+    href: string;
+    label: string;
+    icon: LucideIcon;
+};
+
+export const NavItems: NavItem[] = [
+    { href: "/bearsflight", label: "Radar", icon: Radar},
+    { href: "bearsflight/live", label: "Live", icon: PlaneTakeoff },
+    { href: "/bearsflight/history", label: "History", icon: Clock},
+    { href: "bearsflight/about", label: "About", icon: Globe2 },
+]
