1241c1ab5ece4606c18a80a68657e6b07ddd4d1e
Reketino <bjornevensk8@gmail.com>
2026-02-06 12:58:26 +0100

    Refactor: flight logic in polyline setup

--- src/components/Map/FlightRoute.tsx
+++ src/components/Map/FlightRoute.tsx
@@ -1,3 +1,45 @@
 import { Polyline } from "react-leaflet";
 import type { LatLngExpression } from "leaflet";
 import { useMemo } from "react";
+
+type FlightRoute = {
+    flight: {
+        latitude: number;
+        longitude: number;
+    } | null;
+
+    departureAirport: {
+        lat: number;
+        lon: number;
+    } | null;
+
+    arrivalAirport?: {
+        lat: number;
+        lon: number;
+    } | null
+};
+
+export function FlightRoute({
+    flight, 
+    departureAirport,
+    arrivalAirport
+}: FlightRoute) {
+    const position = useMemo<LatLngExpression[] | null>(() => {
+        if(!flight || !departureAirport) return null;
+
+        const pts: LatLngExpression[] = [
+            [departureAirport.lat, departureAirport.lon],
+            [flight.latitude, flight.longitude],
+        ];
+
+        if (arrivalAirport) {
+            pts.push([arrivalAirport.lat, arrivalAirport.lon]);
+        }
+
+        return pts;
+    },[flight, departureAirport, arrivalAirport]);
+
+    if (!position) return null;
+
+
+}
