f22577bceff3318a986fa9408edb60d8c11eff1b
Reketino <bjornevensk8@gmail.com>
2026-02-07 11:02:43 +0100

    Refactor: changing up some spacing & padding

--- src/components/Map/FlightMarker.tsx
+++ src/components/Map/FlightMarker.tsx
@@ -37,17 +37,15 @@ export function FlightMarker({
           className="
               bg-white/5 text-neutral-900
               rounded-3xl
-              p-5 min-w-55
+              p-3.5 min-w-55
               shadow-[0_10px_30px_rgba(0,0,0,0.15)]
               backdrop-blur 
               animate-in fade-in zoom-in-95 duration-150
               "
         >
           <header
-            className="
-                  flex items-center"
-          >
-            <p className="text-lg">✈️</p>
+            className="flex items-center">
+            <p className="text-base">✈️</p>
             <p className="font-semibold text-sm tracking-tight">{flight.callsign ?? flight.icao24}</p>
           </header>
 
@@ -70,8 +68,8 @@ export function FlightMarker({
 
           {/* Departure Airport in popup */}
 
-          <div className="mt-3 pt-3 border-t border-neutral-200">
-            <div className="truncate">
+          <div className="mt-3 pt-3 border-t border-neutral-300">
+            <div className="truncate font-bold">
             From{" "}
             {departureAirport?.name
               ? `${departureAirport.name}${
