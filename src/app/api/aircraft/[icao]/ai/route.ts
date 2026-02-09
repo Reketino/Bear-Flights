546b3b154a8a7ff18247b706bdbbe42aeef68f21
Reketino <bjornevensk8@gmail.com>
2026-02-09 11:34:39 +0100

    Feat: full route connection to gemini & supabase finished🫡

--- src/app/api/aircraft/[icao]/ai/route.ts
+++ src/app/api/aircraft/[icao]/ai/route.ts
@@ -1,13 +1,40 @@
-import { supabase } from "@/db/supabase";
 import { NextResponse } from "next/server";
+import { getSupabaseServerClient } from "@/lib/supabase";
+import { aiAircraftDescription } from "@/lib/gemini";
+import { sup } from "framer-motion/client";
+
 
 export async function GET(
   _req: Request,
   { params }: { params: { icao: string } },
 ) {
 
- const icao = params.icao.toUpperCase();
+  const supabase = getSupabaseServerClient();
+  const icao = params.icao.toUpperCase();
 
-  const data = await supabase
-  return NextResponse.json(data);
+  const { data } = await supabase
+  .from("aircraft_ai_descriptions")
+  .select("description")
+  .eq("icao", icao)
+  .maybeSingle();
+
+  if (data?.description) {
+  return NextResponse.json({
+    icao,
+    description: data.description,
+    chached: true,
+  });
+  }
+
+  const description = await aiAircraftDescription(icao);
+
+  await supabase
+  .from("aircraft_ai_descriptions")
+  .insert({ icao, description });
+
+  return NextResponse.json({
+    icao,
+    description,
+    cached: false,
+  });
 }
