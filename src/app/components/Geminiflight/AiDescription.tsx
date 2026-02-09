6375295fbed86f2d039ac7802036e228c7f13803
Reketino <bjornevensk8@gmail.com>
2026-02-09 10:22:14 +0100

    Style: formatting

--- src/app/components/Geminiflight/AiDescription.tsx
+++ src/app/components/Geminiflight/AiDescription.tsx
@@ -3,27 +3,25 @@
 import { useEffect, useState } from "react";
 
 type AircraftAI = {
-    icao: string;
-    description: string;
+  icao: string;
+  description: string;
 };
 
-export default function AiDescription({ icao }: {icao: string}) {
-    const [data, setData] = useState<AircraftAI | null>(null);
-    const [loading, setLoading] = useState(true);
+export default function AiDescription({ icao }: { icao: string }) {
+  const [data, setData] = useState<AircraftAI | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=> {
-        async function fetchAI() {
-            const res = await fetch(
-                `/api/aircraft/${icao}/ai`
-            )
-            const json = await res.json();
-            setData(json);
-            setLoading(false);
-        }
+  useEffect(() => {
+    async function fetchAI() {
+      const res = await fetch(`/api/aircraft/${icao}/ai`);
+      const json = await res.json();
+      setData(json);
+      setLoading(false);
+    }
 
-        fetchAI();
-    }, [icao]);
+    fetchAI();
+  }, [icao]);
 
-    if (loading) return <p>Loading Aircraft Info...</p>
-    return <p>{data?.description}</p>
-} 
+  if (loading) return <p>Loading Aircraft Info...</p>;
+  return <p>{data?.description}</p>;
+}
