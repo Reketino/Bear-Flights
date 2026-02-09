6206f863bae54649a8bcf8c3814cea0d2bb3320d
Reketino <bjornevensk8@gmail.com>
2026-02-09 10:35:17 +0100

    Style: formatting

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,13 +1,11 @@
 import { NextResponse } from "next/server";
 
 export async function GET(
-    _req: Request,
-    {params}: {params: {icao:string}}
+  _req: Request,
+  { params }: { params: { icao: string } },
 ) {
-    const res = await fetch(
-        `http://127.0.0.1:8000/aircraft/${params.icao}/ai`
-    );
-    
-    const data = await res.json();
-    return NextResponse.json(data);
+  const res = await fetch(`http://127.0.0.1:8000/aircraft/${params.icao}/ai`);
+
+  const data = await res.json();
+  return NextResponse.json(data);
 }
