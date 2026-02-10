ca746d3a6dce524038f0dd2b7ba1794efeeaeb4b
Reketino <bjornevensk8@gmail.com>
2026-02-10 10:37:13 +0100

    Feat: Logic behind aircraft generating in modal set up

--- src/app/components/AiDescModal/AircraftDescriptionModal.tsx
+++ src/app/components/AiDescModal/AircraftDescriptionModal.tsx
@@ -1,2 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+type Props = {
+    aircraftType: string | null;
+    onClose: () => void;
+};
+
+export default function AircraftDescriptionModal({
+    aircraftType,
+    onClose,
+}: Props) {
+    const [description, setDescription] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if(!aircraftType) return;
+    })
+
+    setLoading(true);
+    fetch(`/api/aircraft/${aircraftType}/ai`)
+    .then((res) => res.json())
+    .then((data) => {
+        setDescription(data.description);
+        setLoading(false);
+        });
+    }, [aircraftType]);
+
+      if (!aircraftType) return null;
+
+      return (
+        <main className=""
+      )
