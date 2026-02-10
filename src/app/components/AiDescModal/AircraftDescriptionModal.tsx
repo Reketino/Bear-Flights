30b0abab03b75cf4175858aee4af67b943cdca1e
Reketino <bjornevensk8@gmail.com>
2026-02-10 10:49:30 +0100

    Feat: set up modal for description of aircrafttype in table

--- src/app/components/AiDescModal/AircraftDescriptionModal.tsx
+++ src/app/components/AiDescModal/AircraftDescriptionModal.tsx
@@ -16,7 +16,6 @@ export default function AircraftDescriptionModal({
 
     useEffect(() => {
         if(!aircraftType) return;
-    })
 
     setLoading(true);
     fetch(`/api/aircraft/${aircraftType}/ai`)
@@ -30,5 +29,29 @@ export default function AircraftDescriptionModal({
       if (!aircraftType) return null;
 
       return (
-        <main className=""
+        <main className="fixed inset-0 z-50 bg-black flex items-center justify-center">
+        <section className="bg-zinc-900 max-w-lg w-full p-6 rounded-xl border border-white/10">
+        <label className="flex justify-between items-center mb-4">
+            <h2 className="font-mono text-sky-400">
+                Aircraft: {aircraftType}  
+            </h2>
+            <button
+            onClick={onClose}
+            className="text-white/60 hover:text-white"
+            >
+                X
+            </button>
+        </label>
+
+        {loading && 
+        <p className="text-white/60">Loading aircraft info...</p>}
+
+        {!loading && description && (
+            <p className="text-sm text-white/80 leading leading-relaxed">
+                {description}
+            </p>
+        )}
+        </section>
+        </main>
       )
+    }
