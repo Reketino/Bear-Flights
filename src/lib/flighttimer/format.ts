08e9b6b064ae209b084d67ac133c1f2e78e28af4
Reketino <bjornevensk8@gmail.com>
2026-02-14 11:15:34 +0100

    Feat: set up format function for cleanber code in flightactivitylive

---
+++ src/lib/flighttimer/format.ts
@@ -0,0 +1,10 @@
+
+type Duration = { h: number; m: number; s: number;}
+
+export function formatDuration(seconds: number): Duration  {
+const h = Math.floor(seconds / 3600);
+const m = Math.floor((seconds % 3600) / 60);
+const s = seconds % 60;
+
+return { h, m, s};
+}
