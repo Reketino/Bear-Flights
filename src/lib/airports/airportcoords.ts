7b22e06c2ad3845ae8c44ffdbb780a3a560c69c6
Reketino <bjornevensk8@gmail.com>
2026-02-15 00:14:25 +0100

    Style: formatting, UI, no logic change

--- src/lib/airports/airportcoords.ts
+++ src/lib/airports/airportcoords.ts
@@ -176,18 +176,18 @@ export const AIRPORTS: Record<string, AirPortCoords> = {
     name: "Puerto del Rosario",
   },
 
+  LEBB: {
+    lat: 43.301105,
+    lon: -2.910609,
+    country: "Bilbao",
+    name: "Spain",
+  },
+
   LBLS: {
     lat: 42.633488,
     lon: 23.646295,
     country: "Lesnovo",
     name: "Bulgaria",
-  },
-
-    LEBB: {
-    lat: 43.301105,
-    lon:  -2.910609,
-    country: "Bilbao",
-    name: "Spain",
   },
 
   // UNITED STATES OF AMERICA
@@ -221,12 +221,10 @@ export const AIRPORTS: Record<string, AirPortCoords> = {
   },
 
   // Asia
-    RJTT: {
+  RJTT: {
     lat: 35.548064,
     lon: 139.777595,
     country: "Japan",
     name: "Tokyo",
   },
-
-
 };
