3651d51635b52fe103c82c88c14dbdcbed7816c6
Reketino <bjornevensk8@gmail.com>
2026-02-20 12:40:28 +0100

    Feat: Added logo for norwegian Refactor: motion section to include logo for aircrafts in modal

--- src/app/components/AiDescModal/AirlineDescriptionModal.tsx
+++ src/app/components/AiDescModal/AirlineDescriptionModal.tsx
@@ -29,6 +29,8 @@ export default function AirlineDescriptionModal({ airlineIcao, onClose }: Props)
 
   const cleanIcao = airlineIcao.trim().toUpperCase();
 
+  const logoUrl = `airlinelogos/${cleanIcao}.svg`
+
   return (
     <AnimatePresence>
       <motion.main
@@ -64,11 +66,12 @@ export default function AirlineDescriptionModal({ airlineIcao, onClose }: Props)
           <div
             className="
           absolute inset-0
-          flex items-center justify-center
-          opacity-10
-          pointer-events-none
+          bg-center bg-no-repeat
+          bg-contain opacity-10
           "
-          
+          style={{
+            backgroundImage: `url('/airlinelogos/${cleanIcao}.svg')`,
+          }}
           />
 
           <div className="relative z-10">
