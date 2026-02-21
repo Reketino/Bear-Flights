86215046586f6a3341f93f254615826cf34d15ac
Reketino <bjornevensk8@gmail.com>
2026-02-21 10:45:17 +0100

    Feat: setup sortAirports function in sortAirports

--- src/lib/airports/sortAirports.ts
+++ src/lib/airports/sortAirports.ts
@@ -1,10 +1,21 @@
 import fs from "fs"
 import path from "path"
 import { AIRPORTS, AirPortCoords } from "./airportcoords"
+import { object } from "framer-motion/client";
 
 function compareAirports(a: AirPortCoords, b: AirPortCoords): number {
     const byCountry = a.country.localeCompare(b.country);
     if (byCountry !== 0) return byCountry;
 
     return (a.name ?? "").localeCompare(b.name ?? "");
+}
+
+function sortAirports(
+    airports: Record<string, AirPortCoords>
+): Record<string, AirPortCoords> {
+    return Object.fromEntries(
+        Object.entries(airports).sort(([, a], [, b]) => 
+        compareAirports(a,b)
+        )
+    );
 }
