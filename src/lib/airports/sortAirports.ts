350094ffb88716eb2c328d722b1b3d6fb62e989d
Reketino <bjornevensk8@gmail.com>
2026-02-21 10:54:05 +0100

    Feat: set up generateFileContent

--- src/lib/airports/sortAirports.ts
+++ src/lib/airports/sortAirports.ts
@@ -2,6 +2,7 @@ import fs from "fs"
 import path from "path"
 import { AIRPORTS, AirPortCoords } from "./airportcoords"
 import { object } from "framer-motion/client";
+import { json } from "stream/consumers";
 
 function compareAirports(a: AirPortCoords, b: AirPortCoords): number {
     const byCountry = a.country.localeCompare(b.country);
@@ -18,4 +19,22 @@ function sortAirports(
         compareAirports(a,b)
         )
     );
+}
+
+function generateFileContent(
+    airports: Record<string, AirPortCoords>
+): string {
+    return `export type AirPortCoords = {
+    lat: number;
+    lon: number;
+    country: string;
+    name?: string;
+    }
+    
+    export const AIRPORTS: Record<string, AirPortCoords> = ${JSON.stringify(
+        airports,
+        null,
+        2
+    )};
+    `;
 }
