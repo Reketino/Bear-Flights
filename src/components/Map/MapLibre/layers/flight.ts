8a4c421f286c297af88844b6d28cc48b8224900e
Reketino <bjornevensk8@gmail.com>
2026-04-27 00:07:38 +0200

    Style: formatting, UI, no logic change

--- src/components/Map/MapLibre/layers/flight.ts
+++ src/components/Map/MapLibre/layers/flight.ts
@@ -2,23 +2,23 @@ import maplibregl from "maplibre-gl";
 import type { FlightPosition } from "@/types/flightposition";
 
 export function addFlightShadow(map: maplibregl.Map) {
-    map.addLayer({
-          id: "flight-shadow",
-        type: "circle",
-        source: "flights",
-        paint: {
-          "circle-radius": [
-            "interpolate",
-            ["linear"],
-            ["get", "altitude"],
-            0,
-            2,
-            10000,
-            14,
-          ],
-          "circle-color": "#000",
-          "circle-opacity": 0.25,
-          "circle-blur": 1.5,
-        },
-    });    
+  map.addLayer({
+    id: "flight-shadow",
+    type: "circle",
+    source: "flights",
+    paint: {
+      "circle-radius": [
+        "interpolate",
+        ["linear"],
+        ["get", "altitude"],
+        0,
+        2,
+        10000,
+        14,
+      ],
+      "circle-color": "#000",
+      "circle-opacity": 0.25,
+      "circle-blur": 1.5,
+    },
+  });
 }
