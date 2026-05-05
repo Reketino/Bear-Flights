906900864d1a5fbe3bd31fde55f23fefbf6c2e56
Reketino <bjornevensk8@gmail.com>
2026-05-06 00:03:17 +0200

    Feat: setup function & logic to addRouteLayer in flightroute.ts

--- src/components/Map/MapLibre/layers/flightroute.ts
+++ src/components/Map/MapLibre/layers/flightroute.ts
@@ -1,2 +1,6 @@
 import maplibregl from "maplibre-gl";
 import type { Feature, LineString } from "geojson";
+
+export function addRouteLayer(map: maplibregl.Map, emptyLine: () => Feature<LineString>) {
+    
+}
