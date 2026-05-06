6b0c7c1b78696c540953319f97c445e9efb83d49
Reketino <bjornevensk8@gmail.com>
2026-05-07 00:04:33 +0200

    Feat: setup map.addSourc w/geojson & emptyline in flightroute.ts

--- src/components/Map/MapLibre/layers/flightroute.ts
+++ src/components/Map/MapLibre/layers/flightroute.ts
@@ -4,4 +4,9 @@ import type { Feature, LineString } from "geojson";
 export function addRouteLayer(
   map: maplibregl.Map,
   emptyLine: () => Feature<LineString>,
-) {}
+) {
+  map.addSource("route", {
+    type: "geojson",
+    data: emptyLine(),
+  })
+}
