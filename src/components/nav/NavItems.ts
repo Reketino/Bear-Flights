e76fa47dcfd9b198f314bd89708ba126f3c52495
Reketino <bjornevensk8@gmail.com>
2026-05-14 00:04:43 +0200

    Style: formatting

--- src/components/nav/NavItems.ts
+++ src/components/nav/NavItems.ts
@@ -1,5 +1,5 @@
 import type { LucideIcon } from "lucide-react";
-import { MapIcon, PlaneTakeoff, Clock, } from "lucide-react";
+import { MapIcon, PlaneTakeoff, Clock } from "lucide-react";
 
 // Defining Navitem function
 export type NavItem = {
