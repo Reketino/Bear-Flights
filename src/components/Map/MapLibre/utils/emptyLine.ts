2db90b268271d406ec459c077d94266ab3398ec2
Reketino <bjornevensk8@gmail.com>
2026-05-19 00:02:19 +0200

    Feat: setup emptyline.ts

--- src/components/Map/MapLibre/utils/emptyLine.ts
+++ src/components/Map/MapLibre/utils/emptyLine.ts
@@ -1,1 +1,12 @@
 import type { Feature, LineString } from "geojson";
+
+export function emptyLine(): Feature<LineString> {
+    return {
+        type: "Feature",
+        properties: {},
+        geometry: {
+            type: "LineString",
+            coordinates: [],
+        }
+    }
+}
