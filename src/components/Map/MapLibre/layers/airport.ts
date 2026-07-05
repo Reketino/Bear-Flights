7ae64aa17d58526c7b09e3e9fae4746fd4fa3dee
Reketino <bjornevensk8@gmail.com>
2026-07-06 00:07:28 +0200

    feat: added airports marker to airport.ts

--- src/components/Map/MapLibre/layers/airport.ts
+++ src/components/Map/MapLibre/layers/airport.ts
@@ -1,3 +1,22 @@
+import maplibregl from "maplibre-gl";
+
 export function addAirportLayers(map: maplibregl.Map) {
+        map.addLayer({
+        id: "airport-markers",
+        type: "circle",
+        source: "airports",
+        paint: {
+          "circle-radius": 8,
+
+          "circle-color": [
+            "match",
+            ["get", "type"],
+            "departure",
+            "#22c55e",
+            "#ef4444",
+          ],
+          "circle-stroke-color": "#ffffff",
+        },
+      });
     
 }
